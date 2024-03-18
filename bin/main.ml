8bf727723331034ba7bd1112c6424c1579a2a756
Michael Dales <michael@digitalflapjack.com>
2024-03-18 16:32:16 +0000

    Bodgy way to get file args from commands

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,46 @@
 open Astring
 
+
+module Command = struct
+  type t = {
+    name : string;
+    args : string list;
+    file_args : string list;
+  }
+
+  let find_file_args args =
+    (* gross liberties, we assume for now that any arg with a doubeldash might be a file. though ultimately this
+       will have to rely on convention, annotation, or guesses, so it's not exactly that bad, just limited as is. I imagine
+       we can have a common prefix for all files, like example.com should be used for domains. *)
+    List.filter (fun arg ->
+      let regex = Str.regexp "^/data" in
+      Str.string_match regex arg 0
+    ) args
+
+  let parse_python_command args =
+    let name = match args with
+    | "-m" :: x :: _ -> x
+    | x :: _ -> x
+    | _ -> "Unknown"
+    in
+    { name = name ; args = args ; file_args = find_file_args args }
+
+  let parse_generic_commmand name args =
+    { name = name ; args = args; file_args = find_file_args args }
+
+  let of_string command_str =
+    (* one day this will be all generic, but for now I'm hardcoding python just to make progress *)
+    match (String.cuts ~sep:" " command_str) with
+    | [] -> None
+    | "python3" :: args -> Some (parse_python_command args)
+    | name :: args -> Some (parse_generic_commmand name args)
+
+  let name c = c.name
+
+  let file_args c = c.file_args
+end
+
+
 module Block = struct
   type t = {
     kind : [ `Build | `Run  ];
@@ -21,16 +62,22 @@ module Block = struct
   let body b = b.body
 
   let command_list b =
-    let regex_newline = Str.regexp "\\\\\n" and regex_comment = Str.regexp "#.*$" in
+    let regex_newline = Str.regexp "\\\\\n"
+    and regex_comment = Str.regexp "#.*$"
+    and regex_whitespace = Str.regexp "[\t ]+" in
     Str.global_replace regex_newline "" b.body |>
     Str.global_replace regex_comment "" |>
     String.cuts ~sep:"\n" |>
+    List.map String.trim |>
+    List.map (Str.global_replace regex_whitespace " ") |>
     List.filter_map (fun l ->
       match l with
       | "" -> None
       | x -> Some x
     )
 end
+
+
 
 let parse_frontmatter frontmatter =
   match (String.trim frontmatter |> Yaml.of_string) with
@@ -74,11 +121,22 @@ let parse_sharkdown file_path =
     | [markdown] -> parse_markdown markdown
     | _ -> failwith "Malformed frontmatter/markdown file"
     in
-    List.iter (fun b ->
-      List.iter (fun c ->
-        Printf.printf "%s\n" c
-      ) (Block.command_list b)
-    ) ast
+    (* Initially ignore block scoping *)
+    List.map Block.command_list ast |>
+    List.concat |>
+    List.map Command.of_string |>
+    List.iter (fun c ->
+      match c with
+      | None -> Printf.printf "lost a command"
+      | Some c -> (
+        Printf.printf "%s\n" (Command.name c);
+        List.iter (
+          fun a ->
+            Printf.printf "\t%s\n" a
+        ) (Command.file_args c);
+      )
+    )
+
 
 let () =
   Eio_main.run @@ fun env -> (
